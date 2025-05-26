e0ea4017c8c9373b719a2809957956fcd2debf40
Livia <livia.zane@andnet.ro>
2025-05-26 14:50:55 +0300

    img pt orase

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -4,14 +4,19 @@ import { fetchHighways, fetchBorders } from '../services/api';
 import BorderLayer from "./BorderLayer";
 import HighwayLayer from "./HighwayLayer";
 import NationalRoadsLayer from "./NationalRoadsLayer";
+import bucurestiImg from '../assets/img/bucuresti.jpg';
+import clujImg from '../assets/img/cluj.jpg';
+import timisoaraImg from '../assets/img/timisoara.jpg';
+import iasiImg from '../assets/img/iasi.jpg';
+import brasovImg from '../assets/img/brasov.jpg';
 
- const cities = [
-   { id: 1, name: "București", latitude: 44.4268, longitude: 26.1025, image_url: "https://upload.wikimedia.org/wikipedia/commons/9/9f/Bucharest_Universitate_square.jpg" },
-   { id: 2, name: "Cluj-Napoca", latitude: 46.7712, longitude: 23.6236, image_url: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Cluj-Napoca_Central_Square.jpg" },
-   { id: 3, name: "Timișoara", latitude: 45.7489, longitude: 21.2087, image_url: "https://upload.wikimedia.org/wikipedia/commons/8/8b/Piata_Victoriei_Timisoara.jpg" },
-   { id: 4, name: "Iași", latitude: 47.1585, longitude: 27.6014, image_url: "https://upload.wikimedia.org/wikipedia/commons/3/3b/Palatul_Culturii_Iasi.jpg" },
-   { id: 5, name: "Brașov", latitude: 45.6579, longitude: 25.6012, image_url: "https://upload.wikimedia.org/wikipedia/commons/2/2e/Brasov_Piata_Sfatului.jpg" },
- ];
+const cities = [
+  { id: 1, name: "București", latitude: 44.4268, longitude: 26.1025, image_url: bucurestiImg },
+  { id: 2, name: "Cluj-Napoca", latitude: 46.7712, longitude: 23.6236, image_url: clujImg },
+  { id: 3, name: "Timișoara", latitude: 45.7489, longitude: 21.2087, image_url: timisoaraImg },
+  { id: 4, name: "Iași", latitude: 47.1585, longitude: 27.6014, image_url: iasiImg },
+  { id: 5, name: "Brașov", latitude: 45.6579, longitude: 25.6012, image_url: brasovImg },
+];
 
 const MapView = ({selectedCity}) => {
   const [highways, setHighways] = useState(null);
