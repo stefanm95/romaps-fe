14bb8ea789729cd8c8783a5ed39115fda65e8895
Livia <livia.zane@andnet.ro>
2025-05-26 14:48:15 +0300

    stergere pini si afisarea pinilor corect

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,17 +1,17 @@
 import { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
+import { MapContainer, TileLayer, GeoJSON, Marker, Popup, useMap } from 'react-leaflet';
 import { fetchHighways, fetchBorders } from '../services/api';
 import BorderLayer from "./BorderLayer";
 import HighwayLayer from "./HighwayLayer";
 import NationalRoadsLayer from "./NationalRoadsLayer";
 
-// const cities = [
-//   { id: 1, name: "București", latitude: 44.4268, longitude: 26.1025, image_url: "https://upload.wikimedia.org/wikipedia/commons/9/9f/Bucharest_Universitate_square.jpg" },
-//   { id: 2, name: "Cluj-Napoca", latitude: 46.7712, longitude: 23.6236, image_url: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Cluj-Napoca_Central_Square.jpg" },
-//   { id: 3, name: "Timișoara", latitude: 45.7489, longitude: 21.2087, image_url: "https://upload.wikimedia.org/wikipedia/commons/8/8b/Piata_Victoriei_Timisoara.jpg" },
-//   { id: 4, name: "Iași", latitude: 47.1585, longitude: 27.6014, image_url: "https://upload.wikimedia.org/wikipedia/commons/3/3b/Palatul_Culturii_Iasi.jpg" },
-//   { id: 5, name: "Brașov", latitude: 45.6579, longitude: 25.6012, image_url: "https://upload.wikimedia.org/wikipedia/commons/2/2e/Brasov_Piata_Sfatului.jpg" },
-// ];
+ const cities = [
+   { id: 1, name: "București", latitude: 44.4268, longitude: 26.1025, image_url: "https://upload.wikimedia.org/wikipedia/commons/9/9f/Bucharest_Universitate_square.jpg" },
+   { id: 2, name: "Cluj-Napoca", latitude: 46.7712, longitude: 23.6236, image_url: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Cluj-Napoca_Central_Square.jpg" },
+   { id: 3, name: "Timișoara", latitude: 45.7489, longitude: 21.2087, image_url: "https://upload.wikimedia.org/wikipedia/commons/8/8b/Piata_Victoriei_Timisoara.jpg" },
+   { id: 4, name: "Iași", latitude: 47.1585, longitude: 27.6014, image_url: "https://upload.wikimedia.org/wikipedia/commons/3/3b/Palatul_Culturii_Iasi.jpg" },
+   { id: 5, name: "Brașov", latitude: 45.6579, longitude: 25.6012, image_url: "https://upload.wikimedia.org/wikipedia/commons/2/2e/Brasov_Piata_Sfatului.jpg" },
+ ];
 
 const MapView = ({selectedCity}) => {
   const [highways, setHighways] = useState(null);
@@ -33,6 +33,10 @@ const MapView = ({selectedCity}) => {
             : geojson.type === "FeatureCollection" && geojson.features.some(f => f.geometry?.type === "Point");
         console.log("Highways has Point:", hasPoint(highwaysData));
         console.log("Borders has Point:", hasPoint(bordersData));
+        console.log(
+          "Highways Points:",
+          highwaysData?.features?.filter(f => f.geometry?.type === "Point")
+        );
         setHighways(highwaysData);
         setBorders(bordersData);
       } catch (err) {
@@ -87,9 +91,21 @@ const MapView = ({selectedCity}) => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        {borders && <GeoJSON data={borders} style={{ color: 'green', weight: 2, fillColor: 'transparent', fillOpacity: 0 }} />}
+        {borders && <GeoJSON data={filterOutPoints(borders)} style={{ color: 'green', weight: 2, fillColor: 'transparent', fillOpacity: 0 }} />}
         {highways && Array.isArray(highways) && highways.map((hw, idx) => (
-          <GeoJSON key={idx} data={hw} style={{ color: 'blue', weight: 2 }} />
+          <GeoJSON key={idx} data={filterOutPoints(hw)} style={{ color: 'blue', weight: 2 }} />
+        ))}
+        <NationalRoadsLayer />
+        {cities.map(city => (
+          <Marker
+            key={city.id}
+            position={[city.latitude, city.longitude]}
+          >
+            <Popup>
+              <b>{city.name}</b><br />
+              <img src={city.image_url} alt={city.name} width={180} />
+            </Popup>
+          </Marker>
         ))}
       </MapContainer>
       {loading && (
